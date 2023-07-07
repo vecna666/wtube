a7de4f63c8333d2cf8e465cdfa5a9cc13462558b
vecna666 <130588389+vecna666@users.noreply.github.com>
2023-07-07 21:24:47 +0200

    Update MainActivity.kt

--- app/src/main/java/com/techmania/webviewwithadblocker/MainActivity.kt
+++ app/src/main/java/com/techmania/webviewwithadblocker/MainActivity.kt
@@ -1,18 +1,13 @@
-package com.techmania.webviewwithadblocker
-
 import android.os.Bundle
-import android.webkit.WebResourceResponse
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 import com.monstertechno.adblocker.AdBlockerWebView
 import com.monstertechno.adblocker.AdBlockerWebView.init
 import com.monstertechno.adblocker.util.AdBlocker
 
-
 class MainActivity : AppCompatActivity() {
 
-    lateinit var webView : WebView
+    lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +18,26 @@ class MainActivity : AppCompatActivity() {
         init(this).initializeWebView(webView)
         webView.webViewClient = BrowserHome()
 
-        webView.loadUrl("https://www.android.com/")
+        // Enable DOM Storage
+        val webSettings: WebSettings = webView.settings
+        webSettings.domStorageEnabled = true
 
+        // Enable JavaScript
+        webSettings.javaScriptEnabled = true
+
+        // Enable hardware acceleration
+        webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null)
+
+        webView.loadUrl("https://www.android.com/")
     }
 }
 
 private class BrowserHome : WebViewClient() {
-
     override fun shouldInterceptRequest(view: WebView, url: String): WebResourceResponse? {
-        return if (AdBlockerWebView.blockAds(
-                view,
-                url
-            )
-        ) AdBlocker.createEmptyResource() else super.shouldInterceptRequest(view, url)
+        return if (AdBlockerWebView.blockAds(view, url)) {
+            AdBlocker.createEmptyResource()
+        } else {
+            super.shouldInterceptRequest(view, url)
+        }
     }
 }
